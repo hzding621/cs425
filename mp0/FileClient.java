8f623be07ab5620e98692937565b9438b347ae4f
Haozhen Ding <hzding621@gmail.com>
2015-01-24 16:20:36 -0600

    mp0

---
+++ mp0/FileClient.java
@@ -0,0 +1,74 @@
+import java.net.*;
+import java.io.*;
+public class FileClient {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+            System.err.println(
+                "Usage: java FileClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+        
+
+        try (
+        	Socket socket = new Socket(hostName, portNumber);
+			  
+			BufferedReader in = new BufferedReader( new InputStreamReader(
+			    socket.getInputStream()));
+			// Enable auto-flush:
+			PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter(
+				socket.getOutputStream())), true);
+        )
+        {
+
+        	System.out.println("client: connecting to " + hostName);
+	
+			String msg = in.readLine();
+			String length = null, content = null;
+			if (msg.equals("LENGTH"))
+				length = in.readLine();
+			else {
+				System.out.println("Unknown message format.");
+				out.println("END");
+			}
+			int len = Integer.parseInt(length);
+
+			msg = in.readLine();
+			if (msg.equals("CONTENT")) {
+
+				StringBuilder sb = new StringBuilder();
+		        String line = in.readLine();
+
+		        while (!line.equals("END")) {
+					sb.append(line);
+					sb.append(System.lineSeparator());
+					line = in.readLine();
+		        }
+				content = sb.deleteCharAt(sb.length()-1).toString();
+			}
+			else {
+				System.out.println("Unknown message format.");
+				out.println("END");
+			}
+			
+			System.out.println("client: received " + len + " bytes" + "\n");
+			System.out.print(content);
+			out.println("END");
+
+	    }
+	    catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } 
+        catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
+
+	}
+	
+}
---
+++ mp0/FileServer.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import java.net.*;
+
+class FileServerThread extends Thread {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	private int connection;
+
+	public FileServerThread(Socket s, int c) throws IOException {
+		socket = s;
+		connection = c;
+		in = new BufferedReader( new InputStreamReader( socket.getInputStream()));
+		// Enable auto-flush:
+		out = new PrintWriter( new BufferedWriter( new OutputStreamWriter(
+						socket.getOutputStream())), true);
+		start(); // Calls run()
+	}
+
+	String readFileForClient(String filename) {
+
+		String everything = null;
+	    try (
+	    	BufferedReader br = new BufferedReader(new FileReader(filename));
+	    ){	
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+
+	        while (line != null) {
+	            sb.append(line);
+	            sb.append(System.lineSeparator());
+	            line = br.readLine();
+	        }
+	        everything = sb.toString();
+	        // System.out.println(everything);
+	    } 
+	    catch (FileNotFoundException e) {
+	    	System.err.println("File not found.");
+	    }
+	    catch (IOException e){
+	    	System.err.println("File read error.");
+	    }
+	    return everything;
+	}
+
+	public void run() {
+		try {
+
+			System.out.println("Client " + connection + " connected");
+
+			while (true) {
+				System.out.println("Sending message to client " + connection );
+				String msg = readFileForClient("test.txt");
+
+				out.println("LENGTH");
+				out.println(msg.length());
+
+				out.println("CONTENT");
+				out.println(msg);
+
+				out.println("END");
+				
+				String str = in.readLine();
+				if (str.equals("END")) break;
+			}
+		}
+		catch(IOException e) {
+			System.err.println("IO Exception");
+		} 
+		finally {
+			try {
+				socket.close();
+			} catch(IOException e) {
+				System.err.println("Socket not closed");
+			}
+		}
+	}
+}
+
+public class FileServer {
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+            System.err.println(
+                "Usage: java FileServer <port number>");
+            System.exit(1);
+        }
+
+		int port = Integer.parseInt(args[0]);
+		ServerSocket s = new ServerSocket(port);
+		String serverPort = s.getLocalPort()+"\n";
+		System.out.println("Server Started at Port: " + serverPort);
+
+		int connection = 0;
+		try {
+			while (true) {
+				Socket socket = s.accept();
+				connection++;
+				try {
+					new FileServerThread (socket, connection);
+				} catch(IOException e) {
+					socket.close();
+				} 
+			} 
+		}
+		finally {
+			s.close();
+		}
+	}
+}
