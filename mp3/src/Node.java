f34bf7e2659dca8927bb3822cd425aa6afa584b7
Haozhen Ding <hzding621@gmail.com>
2015-04-27 18:22:40 -0500

    save

--- mp3/src/Node.java
+++ mp3/src/Node.java
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.concurrent.*;
 import java.sql.*;
 
-class NodeServer {
+class NodeServer extends Thread {
     Node _parent;
     int _port;
     public int get_Id() {
@@ -20,15 +20,26 @@ class NodeServer {
     public void run() {
         try (
                 ServerSocket serverSocket = new ServerSocket(_port);
-                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
                 ) {
-            Socket s = serverSocket.accept();
-            String mm = in.readLine();
-            String[] m = mm.split(",");
-            if (m[0].equals("INIT")) {
-                out.println("READY");
-            } else if 
+            while (true) {
+                try (
+                        Socket s = serverSocket.accept();
+                        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                ) {
+                    String mm = in.readLine();
+                    String[] m = mm.split(",");
+                    if (m[0].equals("INIT")) {
+                        out.println("READY");
+                    } else {
+                        switch (m[0]) {
+
+                        }
+                    }
+                } catch (IOException e) {
+                    // Nothing
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,7 +49,7 @@ class NodeServer {
 
 }
 
-class NodeClient {
+class NodeClient extends Thread{
     Node _parent;
     public int get_Id() {
         return _parent.getId();
@@ -48,19 +59,28 @@ class NodeClient {
     }
     public void init() {
         for (int p: Main.getVotingPort(get_Id())) {
-            try (
-                    Socket s = new Socket("127.0.0.1", p);
-                    PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                    ){
-                out.println("INIT");
-                String m = in.readLine();
-                if (m.equals("READY"))
-                    break;
+            while (true) {
+                try (
+                        Socket s = new Socket("127.0.0.1", p);
+                        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                ){
+                    out.println("INIT");
+                    String m = in.readLine();
+                    if (m.equals("READY"))
+                        break;
 
-            } catch (IOException e) {
-                // Retry
+                } catch (IOException e) {
+                    // Retry
+                }
             }
+        }
+        System.err.println(get_Id()+" is ready.");
+    }
+    public void run() {
+        init();
+        while (true) {
+
         }
     }
 }
@@ -70,6 +90,9 @@ public class Node {
     int _id;
     NodeServer _server;
     NodeClient _client;
+
+
+
     public int getId() {
         return _id;
     }
