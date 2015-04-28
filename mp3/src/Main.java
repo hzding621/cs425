282272bea5ba56c4306909e7d6b74260c3fe0853
Haozhen Ding <hzding621@gmail.com>
2015-04-27 20:00:29 -0500

    deadlock version

--- mp3/src/Main.java
+++ mp3/src/Main.java
@@ -9,13 +9,35 @@ import java.io.*;
 import java.util.concurrent.*;
 import java.sql.*;
 
+class Countdown extends Thread{
+    int seconds;
+    public Countdown(int s) {
+        seconds = s;
+    }
+    public void run() {
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.err.println("Program terminated.");
+        System.exit(0);
+    }
+}
+
 public class Main {
 
+    public static final boolean DEBUG = true;
     public static int cs_int;
     public static int next_req;
     public static int tot_exec_time;
     public static int option;
     public static List<Node> _nodes = new ArrayList<Node>();
+
+    public static int getTimeout(int id) {
+        return (id+1) * 500;
+    }
 
     public static int getPort(int id) {
         return 9000 + id;
@@ -27,13 +49,22 @@ public class Main {
         }
         return array;
     }
-
     public static int[] getVotingPort(int id) {
         int[] array = getVotingSet(id);
         for (int i=0; i<4; i++) {
             array[i] = getPort(array[i]);
         }
         return array;
+    }
+    public static int getIndex(int id, int from) {
+        int i = (from + 9 - id) % 9;
+        switch (i) {
+            case 0: return 0;
+            case 1: return 1;
+            case 2: return 2;
+            case 4: return 3;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -53,8 +84,29 @@ public class Main {
         for (int i=0; i<9; i++) {
             _nodes.get(i).startClient();
         }
-        while (true) {
 
+        Countdown c = new Countdown(tot_exec_time);
+        c.start();
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            if (DEBUG) {
+                String m = sc.next();
+                if (m.equals("show")) {
+                    print(-1);
+                }
+            }
         }
+    }
+
+    public static void print(int caller) {
+        StringBuilder p = new StringBuilder();
+        for (Node n : _nodes) {
+            synchronized (n.responseSet) {
+                p.append(n._id+":"+n.granted+","+n.responseSet[0]+n.responseSet[1]+n.responseSet[2]+n.responseSet[3]+"\n");
+            }
+        }
+        String c = caller != -1? ""+caller : "";
+        System.out.print("**********\n"+ c +"\n" + p.toString()+"**********\n");
     }
 }
--- mp3/src/Node.java
+++ mp3/src/Node.java
@@ -4,8 +4,8 @@
 import java.util.*;
 import java.net.*;
 import java.io.*;
-import java.util.concurrent.*;
 import java.sql.*;
+import java.util.concurrent.locks.*;
 
 class NodeServer extends Thread {
     Node _parent;
@@ -32,8 +32,39 @@ class NodeServer extends Thread {
                     if (m[0].equals("INIT")) {
                         out.println("READY");
                     } else {
+                        int from = Integer.parseInt(m[1]);
                         switch (m[0]) {
-
+                            case "REQUEST": {
+                                if (_parent.granted != -1) {
+                                    _parent.queue.add(from);
+                                    _parent.sendMessage(from, "FAIL");
+                                } else {
+                                    _parent.granted = from;
+                                    _parent.sendMessage(from, "GRANT");
+                                }
+                                break;
+                            }
+                            case "GRANT": {
+                                synchronized (_parent.responseSet) {
+                                    _parent.responseSet[Main.getIndex(get_Id(), from)] = 1;
+                                }
+                                break;
+                            }
+                            case "FAIL": {
+                                synchronized (_parent.responseSet) {
+                                    _parent.responseSet[Main.getIndex(get_Id(), from)] = 2;
+                                }
+                                break;
+                            }
+                            case "RELEASE": {
+                                _parent.granted = -1;
+                                if (_parent.queue.isEmpty() == false) {
+                                    int i = _parent.queue.remove();
+                                    _parent.granted = i;
+                                    _parent.sendMessage(i, "GRANT");
+                                }
+                                break;
+                            }
                         }
                     }
                 } catch (IOException e) {
@@ -77,10 +108,70 @@ class NodeClient extends Thread{
         }
         System.err.println(get_Id()+" is ready.");
     }
+    public int entry(int timeout) {
+        for (int i: Main.getVotingSet(get_Id())) {
+            _parent.sendMessage(i, "REQUEST");
+        }
+        long t1 = System.currentTimeMillis();
+        while (true) {
+            long t2 = System.currentTimeMillis();
+            if (t2 - t1 > timeout) {
+//                System.err.println(get_Id()+" Timeout");
+                return -1;
+            }
+            boolean ok;
+            synchronized (_parent.responseSet) {
+                ok = _parent.responseSet[0] == 1 &&
+                        _parent.responseSet[1] == 1 &&
+                        _parent.responseSet[2] == 1 &&
+                        _parent.responseSet[3] == 1;
+            }
+            if ( ok ) {
+                Main.print(get_Id());
+                return 0;
+            }
+        }
+    }
+    public void exitEntry() {
+        synchronized (_parent.responseSet) {
+            for (int i=0; i<4; i++) {
+                if (_parent.responseSet[i] == 1) {
+                    _parent.sendMessage(Main.getVotingSet(get_Id())[i], "RELEASE");
+                    _parent.responseSet[i] = 0;
+                } else {
+                    _parent.responseSet[i] = 0;
+                }
+            }
+        }
+    }
     public void run() {
         init();
         while (true) {
 
+            int k = entry(Main.getTimeout(get_Id()));
+            if (k == -1) {
+                exitEntry();
+                continue;
+            }
+            Time t = new Time(System.currentTimeMillis());
+            System.out.println(t.toString() + " " + get_Id() + " Enter CS");
+            try {
+                Thread.sleep(Main.cs_int);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+            t = new Time(System.currentTimeMillis());
+            System.out.println(t.toString()+" "+get_Id() + " Exit CS");
+            exitEntry();
+            try {
+                Thread.sleep(Main.next_req);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
         }
     }
 }
@@ -90,8 +181,21 @@ public class Node {
     int _id;
     NodeServer _server;
     NodeClient _client;
-
-
+    Lock lock = new ReentrantLock();
+    int[] responseSet = {0,0,0,0};
+    int granted = -1;
+    Queue<Integer> queue = new ArrayDeque<Integer>();
+    public void sendMessage(int id, String message) {
+        try (
+                Socket s = new Socket("127.0.0.1", Main.getPort(id));
+                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                ) {
+            out.println(message+","+_id);
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
 
     public int getId() {
         return _id;
